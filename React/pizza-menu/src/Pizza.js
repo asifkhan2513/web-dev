e8f6476de87f422720790f63bf9fc0dc0560326c
asifkhan2513 <141583278+asifkhan2513@users.noreply.github.com>
2023-11-09 13:00:32 +0400

    pizza -app

---
+++ React/pizza-menu/src/Pizza.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="spinacci pizaa" />
+      <h2>Pizaa spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  );
+}
