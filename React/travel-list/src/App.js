9dbb2a8ca32bcec81685bd1c8b42c5182289c98b
asifkhan2513 <141583278+asifkhan2513@users.noreply.github.com>
2023-11-11 23:34:25 +0400

    setup

--- React/travel-list/src/App.js
+++ React/travel-list/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+// import "./App.css";
+import "./index.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1> 🌴 Far Away 💼</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>Waht do you need for your 😍 trip ?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list"></div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>you have x item o your bag you already pack x item (x%)</em>
+    </footer>
+  );
+}
