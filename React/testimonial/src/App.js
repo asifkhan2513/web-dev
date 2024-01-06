8bc2b65b18fb668945a240417c2943bec8d4cbe7
asifkhan2513 <141583278+asifkhan2513@users.noreply.github.com>
2024-01-06 12:56:15 +0400

    movie componant finish

---
+++ React/testimonial/src/App.js
@@ -0,0 +1,18 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import Testimonial from "./components/Testimonial";
+import reviews from "./data";
+
+function App() {
+  return (
+    <div className="flex flex-col h-[100vh] w-[100vw] justify-center items-center bg-gray-200">
+      <div className=" text-center ">
+        <h1 className=" text-4xl font-bold">Our Testimonial</h1>
+        <div className=" bg-violet-400 h-[4px] w-1/5 mx-auto"></div>
+        <Testimonial reviews={reviews} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
