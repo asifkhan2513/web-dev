3036aa714c5766bf567a91c06f605718da155155
asifkhan2513 <141583278+asifkhan2513@users.noreply.github.com>
2024-01-11 20:35:37 +0400

    calling api

--- React/movie-review/src/index.js
+++ React/movie-review/src/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import StarRating from "./StarRating";
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movi rating {movieRating} star</p>
+    </div>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <StarRating maxRating={5} />
+    <StarRating maxRating={10} /> */}
+    <StarRating
+      maxRating={5}
+      messages={["terrible", "bad", "okay", "good", "amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
 
