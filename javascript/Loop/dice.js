1d433691ddb00dae776f1b57e0643fe66f9ca50d
asifkhan2513 <141583278+asifkhan2513@users.noreply.github.com>
2024-05-06 18:44:06 +0400

    finish

--- javascript/Loop/dice.js
+++ javascript/Loop/dice.js
@@ -1,9 +1,8 @@
-let dice = Math.trunc(Math.random()*6)+1;
-while(dice !== 6){
-    console.log('you rolled a ${dice}');
-    dice = Math.trunc(Math.random()*6)+1;
-    if(dice == 6){
-
-        console.log('loop is about end')
-    }
+let dice = Math.trunc(Math.random() * 6) + 1;
+while (dice !== 6) {
+  console.log(`you rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice == 6) {
+    console.log("loop is about end");
+  }
 }
--- javascript/Loop/factorial.js
+++ javascript/Loop/factorial.js
@@ -1,11 +1,9 @@
 // execute in the web browser
-let n =  prompt("Enter Any Number")
+let n = prompt("Enter Any Number");
 n = Number.parseInt(n);
-fact =1;
-for(let i=1;i<=n;i++){
-    fact = (fact*i);
+
+fact = 1;
+for (let i = 1; i <= n; i++) {
+  fact = fact * i;
 }
-console.log(fact)
-
-
-
+console.log(fact);
